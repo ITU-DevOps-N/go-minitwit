37a980da066705bc64106557cee5ae1c3043d7b4
Henrik <henrikbellsund@gmail.com>
2022-02-10 20:09:07 +0100

    trying alternative way with GORM

--- minitwit.go
+++ minitwit.go
@@ -13,15 +13,17 @@ package main
 import (
 	// "encoding/json"
 	// "fmt"
-	"log"
-    "net/url"
-	"html/template"
-	"net/http"
-	"database/sql"
 
+	"net/http"
+
+	"github.com/jinzhu/gorm"
 	_ "github.com/mattn/go-sqlite3"
-	"github.com/gorilla/mux"
 )
+
+// App struct to hold a database pointer
+type App struct {
+	DB *gorm.DB
+}
 
 // DB table structure
 type follower struct {
@@ -45,79 +47,119 @@ type user struct {
 }
 
 type Timeline struct {
-    Title       string
-    Messages    []message
+	Title    string
+	Messages []message
 }
 
-// configuration
+func (a *App) Initialize(dbDriver string, dbURI string) {
+	db, err := gorm.Open(dbDriver, dbURI)
+	if err != nil {
+		panic("failed to connect database")
+	}
+	a.DB = db
 
-var DATABASE = "./minitwit.db"
-var PER_PAGE = 30
-var DEBUG = true
-var SECRET_KEY = "development key"
-
-// create our little application
-var app = ""
+	// Migrate the schema.
+	a.DB.AutoMigrate(&user{})
+}
 
 func main() {
-	log.Println("starting API server")
-	// create a new router
-	router := mux.NewRouter()
-	log.Println("creating routes")
-	// specify endpoints
-	router.HandleFunc("/", public_timeline)
+	a := &App{}
+	a.Initialize("sqlite3", "test.db")
 
-	http.Handle("/", router)
-
-	//start and listen to requests
-	http.ListenAndServe(":8080", router)
-}
-
-func db() Timeline {
-	db, err := sql.Open("sqlite3", DATABASE)
-	checkErr(err)
-
-    stmt, err := db.Prepare("select message.*, user.* from message, user where message.flagged = 0 and message.author_id = user.user_id order by message.pub_date desc limit ?")
-    checkErr(err)
-
-    rows, err := stmt.Query(PER_PAGE)
-    checkErr(err)
-
-	var message_id int
-	var author_id  int
-	var text       string
-	var pub_date   int
-	var flagged    int
-
-	var user_id  int
-	var username string
-	var email    string
-	var pw_hash  string
-
-    var timeline Timeline
-    timeline.Messages = make([]message, 0)
-	for rows.Next() {
-		err = rows.Scan(&message_id, &author_id, &text, &pub_date, &flagged, &user_id, &username, &email, &pw_hash) //(&uid, &username, &department, &created)
-		checkErr(err)
-		
-        timeline.Messages = append(timeline.Messages, message{message_id, author_id, text, pub_date, flagged})
-    }
-    return timeline
-}
-
-func public_timeline(w http.ResponseWriter, r *http.Request) {
-    // baseURL := *url.URL
-    // fmap := template.FuncMap{
-	// 	"url_for": func(path string) string {
-	// 		return baseURL.String() + path
-	// 	},
-
-	// }
-    template.Must(template.ParseFiles("templates/timeline-test.html")).Execute(w, db())
-}
-
-func checkErr(err error) {
-	if err != nil {
-		panic("failed to connect database " + err.Error())
+	http.HandleFunc("/", a.handler)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
 	}
+
+	defer a.DB.Close()
 }
+
+func (a *App) handler(w http.ResponseWriter, r *http.Request) {
+	// Create a test Star.
+	a.DB.Create(&user{username: "test"})
+
+	// Read from DB.
+	var user1 user
+	a.DB.First(&user1, "name = ?", "test")
+
+	// Write to HTTP response.
+	w.WriteHeader(200)
+	w.Write([]byte(user1.username))
+
+	// Delete.
+	a.DB.Delete(&user1)
+}
+
+/** GIAN MARCO **/
+// // configuration
+
+// var DATABASE = "./minitwit.db"
+// var PER_PAGE = 30
+// var DEBUG = true
+// var SECRET_KEY = "development key"
+
+// // create our little application
+// var app = ""
+
+// func main() {
+// 	log.Println("starting API server")
+// 	// create a new router
+// 	router := mux.NewRouter()
+// 	log.Println("creating routes")
+// 	// specify endpoints
+// 	router.HandleFunc("/", public_timeline)
+
+// 	http.Handle("/", router)
+
+// 	//start and listen to requests
+// 	http.ListenAndServe(":8080", router)
+// }
+
+// func db() Timeline {
+// 	db, err := sql.Open("sqlite3", DATABASE)
+// 	checkErr(err)
+
+// 	stmt, err := db.Prepare("select message.*, user.* from message, user where message.flagged = 0 and message.author_id = user.user_id order by message.pub_date desc limit ?")
+// 	checkErr(err)
+
+// 	rows, err := stmt.Query(PER_PAGE)
+// 	checkErr(err)
+
+// 	var message_id int
+// 	var author_id int
+// 	var text string
+// 	var pub_date int
+// 	var flagged int
+
+// 	var user_id int
+// 	var username string
+// 	var email string
+// 	var pw_hash string
+
+// 	var timeline Timeline
+// 	timeline.Messages = make([]message, 0)
+// 	for rows.Next() {
+// 		err = rows.Scan(&message_id, &author_id, &text, &pub_date, &flagged, &user_id, &username, &email, &pw_hash) //(&uid, &username, &department, &created)
+// 		checkErr(err)
+
+// 		timeline.Messages = append(timeline.Messages, message{message_id, author_id, text, pub_date, flagged})
+// 	}
+// 	return timeline
+// }
+
+// func public_timeline(w http.ResponseWriter, r *http.Request) {
+// 	// baseURL := *url.URL
+// 	// fmap := template.FuncMap{
+// 	// 	"url_for": func(path string) string {
+// 	// 		return baseURL.String() + path
+// 	// 	},
+
+// 	// }
+// 	template.Must(template.ParseFiles("templates/timeline-test.html")).Execute(w, db())
+// }
+
+// func checkErr(err error) {
+// 	if err != nil {
+// 		panic("failed to connect database " + err.Error())
+// 	}
+// }
