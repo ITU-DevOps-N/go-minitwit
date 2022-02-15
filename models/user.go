51bdc6a570e858b2145b1fc4a9109cb3f0988a98
Gianmarco Murru <gianmarcomurru.gm@gmail.com>
2022-02-15 20:37:02 +0100

    Working with the db

--- models/user.go
+++ models/user.go
@@ -1,10 +1,20 @@
 package models
 
+import "time"
+
 type User struct {
 	ID        uint      `json:"id" gorm:"primary_key"`
 	Username  string    `json:"username" gorm:"type:varchar(100);not null"`
 	Email     string    `json:"email" gorm:"type:varchar(100);not null"`
 	Password  string    `json:"password" gorm:"not null"`
-	Messages  []Message `json:"messages" gorm:"foreignKey:AuthorID;"`       // A user can can have many messages
-	Followers []*User   `json:"followers" gorm:"many2many:user_followers;"` // Self-referencing many2many
+	// Messages  []Message `json:"messages" gorm:"foreignKey:AuthorID;"`       // A user can can have many messages
+	Followers []User   `json:"followers" gorm:"many2many:user_followers;"` // Self-referencing many2many
+}
+
+type Message struct {
+	MessageID uint      `json:"messageID" gorm:"primaryKey"`
+	AuthorID  uint      `json:"authorID"`
+	Text      string    `json:"text"`
+	CreatedAt time.Time `json:"createdAt"`
+	Flagged   bool      `json:"flagged"`
 }
