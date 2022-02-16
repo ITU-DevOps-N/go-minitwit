852e8b8cba1f909c839486c47c8169cf8f2c129b
Gianmarco Murru <gianmarcomurru.gm@gmail.com>
2022-02-16 17:39:20 +0100

    Working with the tpl

--- main.go
+++ main.go
@@ -28,8 +28,6 @@ func healtz(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"Status": "Working good :)"})
 }
 
-
-
 func testDB() {
 
 	// Create
@@ -39,21 +37,21 @@ func testDB() {
 	DB.Create(&model.User{Username: "alex", Email: "alex@itu.dk", Password: "123test"})
 	DB.Create(&model.User{Username: "henri", Email: "henri@itu.dk", Password: "123test"})
 
-	DB.Create(&model.Message{AuthorID: 1, Text: "Hello World! My name is Emil Ravn"})
-	DB.Create(&model.Message{AuthorID: 2, Text: "Hello World! My name is Gianmarco"})
-	DB.Create(&model.Message{AuthorID: 3, Text: "Hello World! My name is Tor"})
-	DB.Create(&model.Message{AuthorID: 4, Text: "Hello World! My name is Alex"})
-	DB.Create(&model.Message{AuthorID: 5, Text: "Hello World! My name is Henri"})
+	DB.Create(&model.Message{Author: "emilravn", Text: "Hello World! My name is Emil Ravn"})
+	DB.Create(&model.Message{Author: "gianmarco", Text: "Hello World! My name is Gianmarco"})
+	DB.Create(&model.Message{Author: "tor", Text: "Hello World! My name is Tor"})
+	DB.Create(&model.Message{Author: "alex", Text: "Hello World! My name is Alex"})
+	DB.Create(&model.Message{Author: "henri", Text: "Hello World! My name is Henri"})
 
-	user := model.User{}
-	follower := model.User{}
-	DB.Where("ID = ?", 2).First(&follower)
-	DB.Where("ID = ?", 1).First(&user)
-	user_followers := append(user.Followers, follower) // This still doesn't work
+	// DB.Create(&model.Follow{Follower: 1, Following: 2})
+	// DB.Create(&model.Follow{Follower: 1, Following: 3})
+	// DB.Create(&model.Follow{Follower: 1, Following: 4})
+	// DB.Create(&model.Follow{Follower: 1, Following: 5})
 
-	// user.Followers = append(user.Followers, follower)
-	// DB.Save(&user)
-	DB.Model(&user).Update("Followers", user_followers)
+	// DB.Create(&model.Follow{Follower: 2, Following: 1})
+	// DB.Create(&model.Follow{Follower: 2, Following: 3})
+	// DB.Create(&model.Follow{Follower: 2, Following: 4})
+	// DB.Create(&model.Follow{Follower: 2, Following: 5})
 
 }
 
@@ -65,7 +63,7 @@ func SetupDB() {
 	}
 
 	// Migrate the entire table schema to the file according to our models
-	db.AutoMigrate(&model.User{}, &model.Message{})
+	db.AutoMigrate(&model.User{}, &model.Message{}, &model.Follow{})
 
 	DB = db
 
@@ -76,42 +74,54 @@ func getUsers(c *gin.Context) {
 	var users []model.User
 
 	DB.Find(&users)
-	// model.db.Find(&books)
+
 
 	c.JSON(http.StatusOK, gin.H{"data": users})
 }
 
-func getMessages(c *gin.Context) {
-	var users []model.User
+func getMessages() []map[string]interface{} {
+	var messages []model.Message
 
-	DB.Find(&users)
-	// model.db.Find(&books)
+	DB.Find(&messages)
+	var results []map[string]interface{}
+	DB.Table("messages").Find(&results)
 
-	c.JSON(http.StatusOK, gin.H{"data": users})
+	return results
+	// c.JSON(http.StatusOK, gin.H{"data": messages})
 }
 
-func timeline(c *gin.Context){
-	DB.Where("message.flagged = 0 and message.author_id = user.user_id and (user.user_id = ? or user.user_id in (select whom_id from follower where who_id = ?))")
+func timeline(c *gin.Context) {
+	// c.JSON(http.StatusOK, gin.H{"data": getMessages()})
+	c.HTML(http.StatusOK, "timeline.tpl", gin.H{
+		"title":    "Timeline",
+		"endpoint": "public_timeline",
+		"messages": getMessages(),
+	})
 
 	//  query_db('''
-    //     select message.*, user.* from message, user
-    //     where message.flagged = 0 and message.author_id = user.user_id and (
-    //         user.user_id = ? or
-    //         user.user_id in (select whom_id from follower
-    //                                 where who_id = ?))
-    //     order by message.pub_date desc limit ?''',
-    //     [session['user_id'], session['user_id'], PER_PAGE]))
+	//     select message.*, user.* from message, user
+	//     where message.flagged = 0 and message.author_id = user.user_id and (
+	//         user.user_id = ? or
+	//         user.user_id in (select whom_id from follower
+	//                                 where who_id = ?))
+	//     order by message.pub_date desc limit ?''',
+	//     [session['user_id'], session['user_id'], PER_PAGE]))
 
 }
 
 func main() {
 	r := gin.Default()
+	r.LoadHTMLGlob("templates/*.tpl")
+	r.Static("/css", "static/css")
 
 	SetupDB()
 	r.GET("/info", healtz)
 	r.GET("/", timeline)
+	r.GET("/public_timeline", timeline)
 	r.GET("/users", getUsers)
-	r.GET("/messages", getMessages)
-	
+	r.GET("/messages", (func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"data": getMessages()})
+		}))
+
 	r.Run()
 }
--- models/user.go
+++ models/user.go
@@ -8,13 +8,18 @@ type User struct {
 	Email     string    `json:"email" gorm:"type:varchar(100);not null"`
 	Password  string    `json:"password" gorm:"not null"`
 	// Messages  []Message `json:"messages" gorm:"foreignKey:AuthorID;"`       // A user can can have many messages
-	Followers []User   `json:"followers" gorm:"many2many:user_followers;"` // Self-referencing many2many
+	// Followers []User   `json:"followers" gorm:"many2many:user_followers;"` // Self-referencing many2many
 }
 
 type Message struct {
 	MessageID uint      `json:"messageID" gorm:"primaryKey"`
-	AuthorID  uint      `json:"authorID"`
+	Author  string      `json:"authorID" gorm:"foreignKey:Username;"`
 	Text      string    `json:"text"`
 	CreatedAt time.Time `json:"createdAt"`
 	Flagged   bool      `json:"flagged"`
+}
+
+type Follow struct {
+	Follower uint `json:"follower" gorm:"primaryKey;foreignKey:ID;"`
+	Following uint `json:"following" gorm:"primaryKey;foreignKey:ID;"`
 }
