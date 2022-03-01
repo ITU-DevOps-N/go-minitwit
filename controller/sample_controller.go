b3b9fc092f8d46081f9d91a0ed2b97b4c57aa9c5
Tor Arnth  Petersen <torarnthpetersen@MacBook-Pro-tilhrende-Tor.local>
2022-03-01 15:45:19 +0100

    work in progress

--- controller/sample_controller.go
+++ controller/sample_controller.go
@@ -1,20 +1,19 @@
 package controller
 
 import (
-  "net/http"
-  "github.com/gin-gonic/gin"
-  // rename packe for easier read  
-  models "github.com/ITU-DevOps-N/go-minitwit/models"
-  main "github.com/ITU-DevOps-N/go-minitwit"
+	"github.com/gin-gonic/gin"
+	// rename packe for easier read
+	main "github.com/ITU-DevOps-N/go-minitwit"
+	models "github.com/ITU-DevOps-N/go-minitwit/models"
 )
+
 // Get all Sample object from DB
 func GetSamples(c *gin.Context) {
-  // We access the DB pointer from the package 
-  var records []Group
+	// We access the DB pointer from the package
+	var records []models.User
 
-  var objects []models.Message
+	var objects []models.Message
 
-  results :=main.DB.Find(&messages)
-  
+	results := main.DB.Find(&messages)
 
 }
